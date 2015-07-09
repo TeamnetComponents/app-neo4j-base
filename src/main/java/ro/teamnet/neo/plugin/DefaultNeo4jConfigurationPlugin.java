20d67c16b02c7d3dc847a4198d11608f6cdc38d4
Viorel Tacliciu <viorel.tacliciu@teamnet.ro>
2015-07-09 11:16:12 +0300

    ou refactoring

--- src/main/java/ro/teamnet/neo/plugin/DefaultNeo4jConfigurationPlugin.java
+++ src/main/java/ro/teamnet/neo/plugin/DefaultNeo4jConfigurationPlugin.java
@@ -1,18 +1,8 @@
 package ro.teamnet.neo.plugin;
 
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.springframework.boot.bind.RelaxedPropertyResolver;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-import org.springframework.data.neo4j.cross_store.config.CrossStoreNeo4jConfiguration;
 import org.springframework.data.neo4j.rest.SpringRestGraphDatabase;
-import org.springframework.stereotype.Component;
-import ro.teamnet.bootstrap.plugin.jpa.DefaultPackagesToScanPlugin;
-import ro.teamnet.bootstrap.plugin.jpa.JpaPackagesToScanPlugin;
 import ro.teamnet.neo.config.NeoConfig;
-
-import javax.inject.Inject;
-import java.io.IOException;
 
 public class DefaultNeo4jConfigurationPlugin implements Neo4jConfigurationPlugin{
 
