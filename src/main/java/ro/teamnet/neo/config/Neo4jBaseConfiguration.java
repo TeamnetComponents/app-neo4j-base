8725a9db679736da38ea03a92a9315e4cf311074
Oana.Mihai <oana.mihai@teamnet.ro>
2015-08-07 15:09:37 +0300

    fixed neo config

--- src/main/java/ro/teamnet/neo/config/Neo4jBaseConfiguration.java
+++ src/main/java/ro/teamnet/neo/config/Neo4jBaseConfiguration.java
@@ -4,6 +4,7 @@ import org.neo4j.graphdb.GraphDatabaseService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.neo4j.cross_store.config.CrossStoreNeo4jConfiguration;
 import org.springframework.plugin.core.PluginRegistry;
@@ -11,21 +12,23 @@ import ro.teamnet.neo.plugin.Neo4JType;
 import ro.teamnet.neo.plugin.Neo4jConfigurationPlugin;
 import ro.teamnet.neo.plugin.NeoPackagesToScanPlugin;
 
+import javax.persistence.EntityManagerFactory;
 import java.util.ArrayList;
 import java.util.List;
 
 @SuppressWarnings("SpringFacetCodeInspection")
 @Configuration
 @Import(Neo4JBasePluginConfiguration.class)
+@DependsOn("entityManagerFactory")
 public class Neo4jBaseConfiguration extends CrossStoreNeo4jConfiguration {
-
 
     @Bean
     public GraphDatabaseService graphDatabaseService(
             @Qualifier("neo4jConfigurationPluginRegistry")
             PluginRegistry<Neo4jConfigurationPlugin, Neo4JType> neo4jConfigurationPluginRegistry,
             @Qualifier("neoPackagesToScanPluginRegistry")
-            PluginRegistry<NeoPackagesToScanPlugin, Neo4JType> neoPackagesToScanPluginRegistry
+            PluginRegistry<NeoPackagesToScanPlugin, Neo4JType> neoPackagesToScanPluginRegistry,
+            EntityManagerFactory emf
     ) {
 
         List<NeoPackagesToScanPlugin> defaultNeoPackagesToScanPlugins=neoPackagesToScanPluginRegistry.getPluginsFor(Neo4JType.PACKAGE_TO_SCAN);
@@ -35,6 +38,8 @@ public class Neo4jBaseConfiguration extends CrossStoreNeo4jConfiguration {
         }
 
         super.setBasePackage(neoPackages.toArray(new String[neoPackages.size()]));
+        setEntityManagerFactory(emf);
+
         return neo4jConfigurationPluginRegistry.getPluginFor(
                 Neo4JType.NEO_4J_CONFIGURATION,
                 neo4jConfigurationPluginRegistry.getPluginFor(
